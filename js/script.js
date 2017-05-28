dfb52406575a0bcdc589c48b2378169bc424ca97
ml4963 <ml4963@nyu.edu>
2017-05-27 23:57:55 -0400

    updating portfolio site

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-$(document).ready(function() {
-
-	$('.toggle-nav').click(function(e) {
-		$(this).toggleClass('active');
-		$('.nav-main ul').toggleClass('active');
-		e.preventDefault();
-	});
-
-	$(".message").typed({
-          strings: ["like to program.", "am part of Tech@NYU's infrastructure team.", "am a hackathon enthusiast.", "live, breathe and drink coffee.", "organized a hackathon called Social Hacks!", "designed and created NYU SASE's website.", "design flyers for Poly Anime Society."],
-          typeSpeed: 60,
-          shuffle: true,
-          loop: true,
-          contentType: 'text'
-    });	
+$(document).ready(function($){
+    $(".fancybox").attr("rel","gallery").fancybox();
 });
